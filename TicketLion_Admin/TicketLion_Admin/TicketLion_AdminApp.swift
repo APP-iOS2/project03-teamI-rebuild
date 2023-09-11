d2708f154d4387bd2d27282bd7aa7e409ba8ad6f
JCxxn <jongchan7298@naver.com>
2023-09-11 23:42:51 +0900

    add: New Workspace

---
+++ TicketLion_Admin/TicketLion_Admin/TicketLion_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketLion_AdminApp.swift
+//  TicketLion_Admin
+//
+//  Created by 김종찬 on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct TicketLion_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicketLion_Consumer/TicketLion_Consumer/TicketLion_ConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketLion_ConsumerApp.swift
+//  TicketLion_Consumer
+//
+//  Created by 김종찬 on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct TicketLion_ConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
